dbae246aabb326ff7eb8881588fb83f8b8dc41ce
huxiaoou <516984451@qq.com>
2024-09-20 18:06:22 +0800

    Revert NOI and NDOI factor to long-position-only

--- solutions/factorAlg.py
+++ solutions/factorAlg.py
@@ -583,46 +583,11 @@ class __CFactorMbrPos(CFactorRaw):
         self.using_diff = cfg.factor_class in ["NDOI", "WNDOI"]
         super().__init__(factor_class=cfg.factor_class, factor_names=cfg.factor_names, **kwargs)
 
-    # def cal_core(self, pos_data: pd.DataFrame, top: int, instru_oi_data: pd.DataFrame) -> pd.DataFrame:
-    #     def __robust_rate(z: pd.Series) -> float:
-    #         return z.iloc[0] / z.iloc[1] * 100 if z.iloc[1] > 0 else np.nan
-    #
-    #     cntrct_pos_data = pos_data.query("code_type == 0")
-    #
-    #     lng_pos_data = cntrct_pos_data[["trade_date", "ts_code", "broker", "long_hld", "long_chg"]].dropna(
-    #         subset=["long_hld", "long_chg"], how="any")
-    #     lng_rnk_data = lng_pos_data[["trade_date", "ts_code", "long_hld"]].groupby(
-    #         by=["trade_date", "ts_code"]).rank(ascending=False)
-    #     lng_data = pd.merge(
-    #         left=lng_pos_data, right=lng_rnk_data,
-    #         left_index=True, right_index=True,
-    #         how="left", suffixes=("", "_rnk")
-    #     ).sort_values(by=["trade_date", "ts_code", "long_hld_rnk"], ascending=True)
-    #     lng_data_slc = lng_data.query(f"long_hld_rnk <= {top}")
-    #
-    #     oi_df = pd.pivot_table(
-    #         data=lng_data_slc,
-    #         index="trade_date",
-    #         values="long_chg" if self.using_diff else "long_hld",
-    #         aggfunc=auto_weight_sum if self.call_weight_sum else "sum",
-    #     )
-    #
-    #     noi_df = pd.merge(
-    #         left=instru_oi_data.set_index("trade_date"), right=oi_df,
-    #         left_index=True, right_index=True, how="left",
-    #     )
-    #     if self.using_diff:
-    #         noi_df["noi_sum"] = noi_df["long_chg"]  # -oi_df["short_chg"]
-    #     else:
-    #         noi_df["noi_sum"] = noi_df["long_hld"]  # -oi_df["short_hld"]
-    #
-    #     noi_df["net"] = noi_df[["noi_sum", "oi_instru"]].apply(__robust_rate, axis=1)
-    #     return noi_df[["net"]]
+    @staticmethod
+    def __robust_rate(z: pd.Series) -> float:
+        return z.iloc[0] / z.iloc[1] * 100 if z.iloc[1] > 0 else np.nan
 
     def cal_core(self, pos_data: pd.DataFrame, top: int, instru_oi_data: pd.DataFrame) -> pd.DataFrame:
-        def __robust_rate(z: pd.Series) -> float:
-            return z.iloc[0] / z.iloc[1] * 100 if z.iloc[1] > 0 else np.nan
-
         cntrct_pos_data = pos_data.query("code_type == 0")
 
         lng_pos_data = cntrct_pos_data[["trade_date", "ts_code", "broker", "long_hld", "long_chg"]].dropna(
@@ -668,11 +633,11 @@ class __CFactorMbrPos(CFactorRaw):
             right=srt_oi_df, left_index=True, right_index=True, how="left",
         )
         if self.using_diff:
-            noi_df["noi_sum"] = noi_df["long_chg"] - noi_df["short_chg"]
+            noi_df["noi_sum"] = noi_df["long_chg"]  # - noi_df["short_chg"]
         else:
-            noi_df["noi_sum"] = noi_df["long_hld"] - noi_df["short_hld"]
+            noi_df["noi_sum"] = noi_df["long_hld"]  # - noi_df["short_hld"]
 
-        noi_df["net"] = noi_df[["noi_sum", "oi_instru"]].apply(__robust_rate, axis=1)
+        noi_df["net"] = noi_df[["noi_sum", "oi_instru"]].apply(self.__robust_rate, axis=1)
         return noi_df[["net"]]
 
     def cal_factor_by_instru(
